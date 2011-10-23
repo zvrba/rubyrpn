2a3b021fc0b42aa70791f7f434a6ec4120a610cb
zvrba <zvrba.external@zvrba.net>
2011-10-23 18:31:55 +0200

    Finished RPL syntax; added small interactive test program.

--- RPLSyntax.rb
+++ RPLSyntax.rb
@@ -1,18 +1,33 @@
 require 'parslet'
 
 class RPLSyntax < Parslet::Parser
-  rule (:skipws)  { match('\s').repeat }
-
-  rule (:decnat)   { match('[0-9]').repeat(1) }
+  # Low-level lexical elements
+  rule (:ws)       { match('\s').repeat }
+  rule (:decdigit) { match('[0-9]') }
+  rule (:decnat)   { decdigit.repeat(1) }
   rule (:decint)   { match('-').maybe >> decnat }
   rule (:binint)   { match('0') >> match('[bB]') >> match('[01]').repeat(1) }
   rule (:hexint)   { match('0') >> match('[xX]') >> match('[0-9a-fA-F]').repeat(1) }
   rule (:intnum)   { binint | hexint | decint }
   rule (:floatexp) { match('[eE]') >> decint }
   rule (:flonum)   { decint >> match('[.]') >> decnat >> floatexp.maybe }
-  rule (:number)   { flonum.as(:flonum) | intnum.as(:intnum) }
 
-  root (:number)
+  # Lexical atoms -- :flonum, :intnum, :var, :op
+  rule (:number)   { (flonum.as(:flonum) | intnum.as(:intnum)) }
+  rule (:var)      { (match("'") >> (match('[a-zA-Z0-9_]').repeat(1))).as(:var) }
+  rule (:op)       { (match('[+*/a-zA-Z!@$%^&=|~<>?-]').repeat(1)).as(:op) }
+  rule (:atom)     { number | var | op }
+
+  # Composites -- vector, matrix, list
+  rule (:vector)   { match('\[') >> ws >> (number >> ws).repeat(1).as(:vector) >> match('\]') }
+  rule (:matrix)   { match('\[') >> ws >> (vector >> ws).repeat(1).as(:matrix) >> match('\]') }
+  rule (:list)     { match('\{') >> ws >> (atom >> ws).repeat(1).as(:list) >> match('}') }
+  rule (:comp)     { vector | matrix | list }
+
+  # Acceptable input
+  rule (:input)    { ((atom | comp) >> ws).repeat(1) }
+
+  root (:input)
 end
 
 if $0 == __FILE__ then
