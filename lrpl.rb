7e0e81b5995e3a793b8cd077dadc43a17ffaef30
Zeljko Vrba <zvrba@zvrba.net>
2011-11-19 16:12:28 +0100

    Added custom formatters.

--- lrpl.rb
+++ lrpl.rb
@@ -12,30 +12,45 @@ class Ledit
     @formats   = {}
     @lasterr   = nil
 
-    @formats[:DEFAULT] = proc { |o| o.inspect }
+    @formats[:DEFAULT]  = "%p"
+    @formats[:WORDSIZE] = 64
+    @formats[Integer]   = "%d"
+    @formats[Float]     = "%f"
   end
 
   def main
     while (line = prompt)
-      begin
-        oldstack = @sequencer.stack.clone
-        xt = @sequencer.compile(line)
-        @sequencer.xt xt
-      rescue
-        @lasterr = "#{$!.to_s} [#{$!.class}]"
-        @sequencer.stack[0..-1] = oldstack
-        #raise
+      if line =~ /^\./ then
+        line = line.split
+        cmd  = "do_#{line[0][1..-1]}"
+        if not self.respond_to?(cmd, true) then
+          @lasterr = "UNKNOWN META-COMMAND: #{line[0]} (#{cmd})"
+        else
+          self.send(cmd.to_sym, line[1..-1])
+        end
+      else
+        begin
+          oldstack = @sequencer.stack.clone
+          xt = @sequencer.compile(line)
+          @sequencer.xt xt
+        rescue
+          @lasterr = "#{$!.to_s} [#{$!.class}]"
+          @sequencer.stack[0..-1] = oldstack
+          #raise
+        end
       end
     end
   end
+
+private
 
   def print_stack
     if @sequencer.stack.empty? then
       puts "--EMPTY STACK--"
     else
       @sequencer.stack.each.with_index do |obj, i|
-        formatter = @formats[obj.class] || @formats[:DEFAULT]
-        puts sprintf("%03d: %s", @sequencer.stack.length-i, formatter.call(obj))
+        format = "%03d: " + find_format(obj)
+        puts sprintf(format, @sequencer.stack.length-i, obj)
       end
     end
   end
@@ -49,6 +64,34 @@ class Ledit
     end
     print "\nRPN> "
     $_.chomp! if gets
+  end
+
+  def find_format(obj)
+    k = obj.class
+    while k != Object
+      return @formats[k] if @formats[k]
+      k = k.superclass
+    end
+    return @formats[:DEFAULT]
+  end
+
+  # COMMAND HANDLERS
+
+  def do_quit(args)
+    exit
+  end
+
+  def do_dec(args)
+    @formats[Integer] = "%d"
+  end
+
+  def do_hex(args)
+    w = @formats[:WORDSIZE] / 4
+    @formats[Integer] = "%0#{w}X"
+  end
+
+  def do_ws(args)
+    @formats[:WORDSIZE] = args[0].to_i
   end
 
 end
